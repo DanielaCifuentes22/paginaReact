cd8ce379abbb3901e2a6144ddd44a0f4eb87a51c
Daniela <dannyc8522@gmail.com>
2026-07-07 22:07:52 -0500

    Se agregó el header del proyecto

---
+++ Documents/ADSO-SENA/proyectoReact/frontend-proyecto-react/src/components/Header.jsx
@@ -0,0 +1,20 @@
+function Header(){
+    return(
+        <header className = "header">
+            <div className = "logo">
+                <img src=".img/logogaleria.png" alt="logotipo" />
+                <img src="img/ubicacion.png" alt="ubicación" />
+            </div>
+            <nav>
+                <a href="#inicio">INICIO</a>
+                <a href="#obras">OBRAS</a>
+                <a href="exposiciones">EXPOSICIONES</a>
+                <a href="tienda">TIENDA</a>
+                <a href="sobre mí">SOBRE MI</a>
+                <a href="talleres">TALLERES</a>
+                <a href="contacto">CONTACTO</a>
+            </nav>
+        </header>
+    );
+}
+export default Header;
