a3f9162309935af1f094ec0e455591cf0898f766
Daniela <dannyc8522@gmail.com>
2026-07-11 12:56:16 -0500

    Se hicieron correcciones en InfoSection

--- Documents/ADSO-SENA/proyectoReact/frontend-proyecto-react/src/components/Hero.jsx
+++ Documents/ADSO-SENA/proyectoReact/frontend-proyecto-react/src/components/Hero.jsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
 import foto1 from "../img/foto1.jpeg";
-import foto2 from "../img/foto2.jpg";
-import foto3 from "../img/foto3.jpg";
+import foto2 from "../img/foto2.JPG";
+import foto3 from "../img/foto3.JPG";
 import foto4 from "../img/foto4.jpg";
 import foto5 from "../img/foto5.jpg";
-import foto6 from "../img/foto6.jpg";
+import foto6 from "../img/foto6.JPG";
 import foto7 from "../img/foto7.jpeg";
 
 function Hero(){
--- Documents/ADSO-SENA/proyectoReact/frontend-proyecto-react/src/components/InfoSection.jsx
+++ Documents/ADSO-SENA/proyectoReact/frontend-proyecto-react/src/components/InfoSection.jsx
@@ -7,21 +7,21 @@ function InfoSection(){
             <div className = "cards-container">
                 <Card
                 titulo = "Objetivo de la galería"
-                descripcion = "Concebir el arte como un espacio de contemplación y transformación,
-                donde cada obra invita a descubrir nuevos significados."
+                descripcion = {`Concebir el arte como un espacio de contemplación y transformación,
+                donde cada obra invita a descubrir nuevos significados.`}
                 />
                 <Card
                 titulo = "Información general"
-                descripcion = "Dirección de la galería: Carrera 30 # 50-69 Bello horizonte Pereira
+                descripcion = {`Dirección de la galería: Carrera 30 # 50-69 Bello horizonte Pereira
                 Teléfono: 6063500000
                 Correo: galeriacuecha@gmail.com
-                Horarios de atención: Lunes a viernes 8:00 am - 12:00 m y de 2:00 pm - 4:00 pm"
+                Horarios de atención: Lunes a viernes 8:00 am - 12:00 m y de 2:00 pm - 4:00 pm`}
                 />
                 <Card 
                 titulo = "Servicios"
-                descripcion = "Se ofrece talleres de pintura y dibujo.
+                descripcion = {`Se ofrece talleres de pintura y dibujo.
                  clases personalizadas en modalidad presencial o virtual.
-                 Se abren cupos con previa inscripción y pago del 50 % del servicio."
+                 Se abren cupos con previa inscripción y pago del 50 % del servicio.`}
                  botonTexto = "Inscribirse"
                 />
             </div>
