c7e783a4d6a058e50214fa7d0a94794d6066487a
Daniela <dannyc8522@gmail.com>
2026-07-11 11:40:19 -0500

    Se agregó el componente ContactForm para que el usuario pueda enviar tipos de solicitudes al area de servicio al cliente

---
+++ Documents/ADSO-SENA/proyectoReact/frontend-proyecto-react/src/components/ContactForm.jsx
@@ -0,0 +1,34 @@
+function ContactForm(){
+    return(
+        <section className = "section" id = "contacto">
+            <h2>Formulario de contacto</h2>
+            <form className = "form" action="">
+                <label htmlFor="">
+                    Nombre:
+                    <input type="text" placeholder="Escriba su nombre completo"/>
+                </label>
+                <label htmlFor="">
+                    Correo:
+                    <input type="text" placeholder="Escriba su correo" />
+                </label>
+                <label>
+                    Tipo de solicitud:
+                    <select>
+                        <option value="">Seleccione una opción</option>
+                        <option value="pqr">PQR</option>
+                        <option value="inscripcion">Inscripción</option>
+                        <option value="soporte">Soporte técnico</option>
+                        <option value="entrevista">Entrevista</option>
+                        <option value="colaboracion">Colaboración</option>
+                    </select>
+                </label>
+                <label htmlFor="">
+                    Mensaje:
+                    <input type="text" placeholder="Escribe su mensaje"/>
+                </label>
+                <button type="submit">Enviar</button>
+            </form>
+        </section>
+    );
+}
+export default ContactForm;
