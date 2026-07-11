4720ba9fe882a18ee5ae52d7a0dbdf73dd0d2eac
Daniela <dannyc8522@gmail.com>
2026-07-11 12:23:15 -0500

    Se agrego el componente Footer donde estarán las redes sociales del artista de la página, donde el usuario es redireccionado al dar clic en cada ícono

---
+++ Documents/ADSO-SENA/proyectoReact/frontend-proyecto-react/src/components/Footer.jsx
@@ -0,0 +1,40 @@
+import facebook from "../img/facebook.png";
+import instagram from "../img/instagram.png";
+import whatsapp from "../img/whatsapp.png";
+
+function Footer(){
+    return(
+        <footer className="footer">
+            <p>© 2026 Galería Cuecha - Todos los derechos reservados.</p>
+            <div className="redes-sociales">
+
+                <a
+                    href="https://www.facebook.com/share/19LC8ncekp/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <img src={facebook} alt="Facebook" />
+                </a>
+
+                <a
+                    href="https://www.instagram.com/cuecha_?igsh=Y3l1MjFrMTVkZ2d3"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <img src={instagram} alt="Instagram" />
+                </a>
+
+                <a
+                    href="https://wa.me/573106144502"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <img src={whatsapp} alt="Whatsapp" />
+                </a>
+
+            </div>
+
+        </footer>
+    );
+}
+export default Footer;
