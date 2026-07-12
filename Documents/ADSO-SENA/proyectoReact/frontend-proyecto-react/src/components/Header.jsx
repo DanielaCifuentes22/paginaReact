146898986f891470b3445ad9023fcc3469f59792
Daniela <dannyc8522@gmail.com>
2026-07-11 23:29:45 -0500

    se importaron las imagenes del header}

--- Documents/ADSO-SENA/proyectoReact/frontend-proyecto-react/src/components/Header.jsx
+++ Documents/ADSO-SENA/proyectoReact/frontend-proyecto-react/src/components/Header.jsx
@@ -1,11 +1,13 @@
 import IdiomaMenu from './IdiomaMenu';
+import logogaleria from "../img/logogaleria.png";
+import ubicacion from "../img/ubicacion.png";
 
 function Header(){
     return(
         <header className = "header">
             <div className = "logo">
-                <img src=".img/logogaleria.png" alt="logotipo" />
-                <img src="img/ubicacion.png" alt="ubicación" />
+                <img src={logogaleria} alt="logo de la galeria"/>
+                <img src={ubicacion} alt="ubicación"/>
             </div>
              <IdiomaMenu />
             <nav>
